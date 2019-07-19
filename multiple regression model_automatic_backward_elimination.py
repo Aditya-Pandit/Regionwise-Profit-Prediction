a713314131c91adce5802dcdf2af22ac881a6d7b
Aditya Shankar Pandit <44620544+Aditya-Pandit@users.noreply.github.com>
2019-07-20 01:51:45 +0530

    Update multiple regression model_automatic_backward_elimination.py

--- multiple regression model_automatic_backward_elimination.py
+++ multiple regression model_automatic_backward_elimination.py
@@ -1,7 +1,6 @@
 # multiple regression model
 
 import numpy as np
-#import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing the dataset
@@ -23,14 +22,6 @@ X=X[:, 1:]
 #splitting thbe dataset into the Training set and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size = 0.1, random_state=0)
-
-#featuring scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train  = sc_X.tranform(X_train)
-X_test = sc_x.transform(x_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
 #fitting the multiple linear regression to the training set
 from sklearn.linear_model import LinearRegression
